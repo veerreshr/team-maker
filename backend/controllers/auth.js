796beabaf49977a806f7ec47baeac9761cbf1902
KshitijDarekar <KshitijDarekar13@gmail.com>
2021-06-26 15:13:23 +0530

    update: controllers for auth and user

---
+++ backend/controllers/auth.js
@@ -0,0 +1,79 @@
+import expressAsyncHandler from "express-async-handler";
+import User from "./../models/userModel.js";
+import generateToken from "./../utils/generateToken.js";
+
+// @desc    Auth user & get token
+// @route   POST /api/users/login
+// @access  Public
+const login = expressAsyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  // //create token
+  // const token = jwt.sign({ _id: user._id }, process.env.SECRET);
+  // // put token in cookie
+  // res.cookie("token",token,{expire:new Date()+9999});
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
+});
+
+
+// @desc    Regsiter new user
+// @route   POST /api/users
+// @access  Public
+const registerUser = expressAsyncHandler(async (req, res) => {
+  // const { name, email, password,skills,languages,about } = req.body;
+  const {email}= req.body;
+  const userExists = await User.findOne({email});
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+  // const user = await User.create({ name, email, password,skills,languages,about });
+  const user = await User.create(req.body);
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+      skills:user.skills,
+      languages:user.languages,
+      gender:user.gender,
+      location:user.location,
+      college:user.college,
+      certification:user.certification,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
+
+//logout
+const logout=(req,res)=>{
+
+  res.clearCookie("token");
+  res.send('User logout Route');
+  res.json({
+      message:"User logout Successfully"
+  })
+
+}
+
+export {
+  login,
+  registerUser,
+  logout,
+};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,19 +1,26 @@
 import express from "express";
 import {
-  authUser,
+  login,
+  registerUser,
+  logout,
+  
+} from "../controllers/auth.js";
+
+import {
   deleteUser,
   getUserProfile,
   getUsers,
-  registerUser,
   updateUserProfile,
   getUserById,
+  getUser,
   updateUser,
-} from "../controllers/userController.js";
+} from "../controllers/user.js";
+
 import { admin, protect } from "../middleware/authMiddleware.js";
 const router = express.Router();
 router.route("/").post(registerUser).get(protect, admin, getUsers);
 
-router.post("/login", authUser);
+router.post("/login", login);
 router
   .route("/profile")
   .get(protect, getUserProfile)
@@ -23,5 +30,22 @@ router
   .delete(protect, admin, deleteUser)
   .get(protect, admin, getUserById)
   .put(protect, admin, updateUser);
+
+//Register Route
+router.post('/register',registerUser)
+//Logout Route
+router.get  ('/logout',logout)
+
+
+// PARAMS FOR USER ROUTES
+
+router.param('UserId', getUserById)
+
+// route to trigger the capture
+router.get('/user/:UserId', getUser)
+
+router.put('/user/:UserId', updateUser)
+// router.get('/users',getAllUsers)
+
 
 export default router;
