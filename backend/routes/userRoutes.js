1d9184a21f5851d20c9514dfb8d889d4ccacb228
veeresh <veerreshr@gmail.com>
2021-09-30 19:44:54 +0530

    added getuserprofile by username route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,29 +1,24 @@
 import express from "express";
-import {
-  login,
-  registerUser,
-  logout,
-  
-} from "../controllers/auth.js";
+import { login, registerUser, logout } from "../controllers/auth.js";
 
 import {
   deleteUser,
   getUserProfile,
   getAllUsers,
   updateUserProfile,
-  getUserById,
-  getUser,
+  getUserProfileByUsername,
   updateUser,
   getTeamById,
 } from "../controllers/user.js";
 
 import { admin, protect } from "../middleware/authMiddleware.js";
+import { buildCheckFunction } from "express-validator";
+const checkQuery = buildCheckFunction(["query"]);
 const router = express.Router();
 // router.route("/").post(registerUser).get(protect, admin, getUsers);
 
-
-// Get data about the User's Team 
-router.get("/getteams",protect,getTeamById);
+// Get data about the User's Team
+router.get("/getteams", protect, getTeamById);
 
 router.post("/login", login);
 router
@@ -36,22 +31,20 @@ router
 //   .get(protect, admin, getUserById)
 //   .put(protect, admin, updateUser);
 
-
 //Register Route
-router.post('/register',registerUser)
+router.post("/register", registerUser);
 //Logout Route
-router.get  ('/logout',logout)
-
+router.get("/logout", logout);
 
 // PARAMS FOR USER ROUTES
 
-router.param('UserId', getUserById)
+router.get(
+  "/getuserprofile",
+  checkQuery("username").not().isEmpty().trim().escape(),
+  getUserProfileByUsername
+);
 
-// route to trigger the capture
-router.get('/:UserId', getUser)
-
-router.put('/:UserId', updateUser)
+router.put("/:UserId", updateUser);
 // router.get('/users',getAllUsers)
-
 
 export default router;
