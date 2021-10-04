dd2e75147e5272c8b96b95ba31e39a57e64ede97
veeresh <veerreshr@gmail.com>
2021-10-05 03:27:11 +0530

    added username response for auth requests in backend

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -6,19 +6,15 @@ import generateToken from "./../utils/generateToken.js";
 // @route   POST /api/users/login
 // @access  Public
 const login = expressAsyncHandler(async (req, res) => {
-
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
-    // //create token
-    // const token = jwt.sign({ _id: user._id }, process.env.SECRET);
-    // // put token in cookie
-    // res.cookie("token",token,{expire:new Date()+9999});
     if (user && (await user.matchPassword(password))) {
       res.json({
         _id: user._id,
         name: user.name,
         email: user.email,
+        username: user.username,
         isAdmin: user.isAdmin,
         token: generateToken(user._id),
       });
@@ -28,30 +24,38 @@ const login = expressAsyncHandler(async (req, res) => {
     }
   } catch (error) {
     res.status(400);
-    throw new Error("Something went wrong "+ error);
+    throw new Error("Something went wrong " + error);
   }
 });
-
 
 // @desc    Regsiter new user
 // @route   POST /api/users
 // @access  Public
 const registerUser = expressAsyncHandler(async (req, res) => {
-  const {email, username, password, name} = req.body;
-  const userExists = await User.findOne({$or: [ { email : email}, { username: username }]});
+  const { email, username, password, name } = req.body;
+  const userExists = await User.findOne({
+    $or: [{ email: email }, { username: username }],
+  });
   if (userExists) {
     res.status(400);
     throw new Error("Username or Email already exists");
   }
 
-  const user = await User.create({email : email, name : name, username : username, password : password});
+  const user = await User.create({
+    email: email,
+    name: name,
+    username: username,
+    password: password,
+  });
 
   if (user) {
     res.status(201).json({
-      username : user.username,
-      name : user.name,
-      email : user.email,
-      token : generateToken(user._id)
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      username: user.username,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
@@ -60,18 +64,12 @@ const registerUser = expressAsyncHandler(async (req, res) => {
 });
 
 //logout
-const logout=(req,res)=>{
-
+const logout = (req, res) => {
   res.clearCookie("token");
-  res.send('User logout Route');
+  res.send("User logout Route");
   res.json({
-      message:"User logout Successfully"
-  })
-
-}
-
-export {
-  login,
-  registerUser,
-  logout,
+    message: "User logout Successfully",
+  });
 };
+
+export { login, registerUser, logout };
