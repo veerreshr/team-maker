fd24e3df1e3dc29de45f600658acc982e0879fac
veeresh <veerreshr@gmail.com>
2021-10-05 03:29:31 +0530

    increased rate limiter to 30 per minute

--- backend/middleware/rateLimiter.js
+++ backend/middleware/rateLimiter.js
@@ -3,7 +3,7 @@ import rateLimit from "express-rate-limit";
 // set up rate limiter: maximum of five requests per minute
 export const limiter = new rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
-  max: 15,
+  max: 30,
   message: "You have exceeded the 5 requests in 1 minute limit!",
   headers: true,
 });
