cd0b848882d15b1fa1e5a06241398a6b0c1a1682
veeresh <veerreshr@gmail.com>
2021-11-07 03:19:42 +0530

    bug: fixed server issue in backend

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,12 @@ import { config } from "dotenv";
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import teamRoutes from "./routes/teamRoutes.js";
-import eventRoutes from './routes/eventRoutes.js';
-import helperRoutes from './routes/helperRoutes.js';
+import eventRoutes from "./routes/eventRoutes.js";
+import helperRoutes from "./routes/helperRoutes.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 import path from "path";
 import morgan from "morgan";
-import { createServer } from 'http';
-import { Server } from 'socket.io';
-import {limiter } from './middleware/rateLimiter.js';
+import { limiter } from "./middleware/rateLimiter.js";
 
 config();
 
@@ -28,13 +26,11 @@ app.use(express.json());
 
 app.use("/api/users", userRoutes);
 
-app.use("/api/teams",teamRoutes);
+app.use("/api/teams", teamRoutes);
 
 app.use("/api/events", eventRoutes);
 
 app.use("/api/helper", helperRoutes);
-
-
 
 const __dirname = path.resolve();
 
@@ -55,7 +51,7 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(
+app.listen(
   PORT,
   console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
