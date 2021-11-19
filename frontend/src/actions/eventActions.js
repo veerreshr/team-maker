b67971b6dd2f291b88d6a8c5b19320300d55671a
Veeresh <59141533+veerreshr@users.noreply.github.com>
2021-11-20 03:06:39 +0530

    Frontend: [Refactor] Events Screen (#31)
    * Frontend: [Bug] Fixed Password visibility bug
    
    * Frontend: [Refactor] Added Events in Homepage with live data
    
    * Frontend: [Typo] Adjusted Events Card typo

--- frontend/src/actions/eventActions.js
+++ frontend/src/actions/eventActions.js
@@ -1,40 +1,67 @@
-import axios from "axios"
+import axios from "axios";
 import {
-    ADD_EVENT_REQUEST,
-    ADD_EVENT_SUCCESS,
-    ADD_EVENT_FAIL
-      } from "../constants/eventConstants";
+  GET_EVENTS_REQUEST,
+  GET_EVENTS_SUCCESS,
+  GET_EVENTS_FAIL,
+  GET_SCRAPPED_EVENTS_REQUEST,
+  GET_SCRAPPED_EVENTS_SUCCESS,
+  GET_SCRAPPED_EVENTS_FAIL,
+} from "../constants/eventConstants";
 
-export const addEventAction = (eventName, eventDesc,startDate) => async (dispatch) => {
-    try {
-      dispatch({
-        type: ADD_EVENT_REQUEST,
-      });
-  
-      const config = {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      };
-  
-      const { data } = await axios.post(
-        "/api/customEvents/addEvent",
-        {eventName, eventDesc,startDate },
-        config
-      );
-  
-      dispatch({
-        type: ADD_EVENT_SUCCESS,
-        payload: data,
-      });
-  
-    } catch (error) {
-      dispatch({
-        type: ADD_EVENT_FAIL,
-        payload:
-          error.response && error.response.data.message
-            ? error.response.data.message
-            : error.message,
-      });
-    }
-  };
+export const getEventsAction = () => async (dispatch) => {
+  try {
+    dispatch({
+      type: GET_EVENTS_REQUEST,
+    });
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const { data } = await axios.get("/api/events", config);
+
+    dispatch({
+      type: GET_EVENTS_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: GET_EVENTS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const getScrappedEventsAction = () => async (dispatch) => {
+  try {
+    dispatch({
+      type: GET_SCRAPPED_EVENTS_REQUEST,
+    });
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const { data } = await axios.get("/api/events/getScrappedEvents", config);
+
+    dispatch({
+      type: GET_SCRAPPED_EVENTS_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: GET_SCRAPPED_EVENTS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
