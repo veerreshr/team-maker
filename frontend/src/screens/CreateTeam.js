824a9149a43e4eb1206bfe11f23c623e92c2cf15
veeresh <veerreshr@gmail.com>
2021-11-24 00:15:47 +0530

    Frontend: created a create team page with dummy data

--- frontend/src/screens/CreateTeam.js
+++ frontend/src/screens/CreateTeam.js
@@ -1,7 +1,99 @@
-import React from "react";
+import React, { useState } from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import InputAdornment from "@mui/material/InputAdornment";
+import IconButton from "@mui/material/IconButton";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import AddIcon from "@mui/icons-material/Add";
 
 function CreateTeam() {
-  return <div>Hello</div>;
+  const [showPassword, setShowPassword] = useState(false);
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  const handleSubmit = () => {};
+  return (
+    <Box
+      sx={{
+        my: 8,
+        mx: 4,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <Avatar sx={{ m: 1, bgcolor: "secondary.main", width: 70, height: 70 }}>
+        <AddCircleIcon fontSize="large" />
+      </Avatar>
+      <Typography component="h1" variant="h5">
+        Create a Team
+      </Typography>
+      <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="name"
+          label="Team Name"
+          name="name"
+          autoComplete="name"
+          autoFocus
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="events"
+          label="Event Names"
+          name="events"
+          autoComplete="events"
+          helperText="Enter comma separated event names that you are participating in. Ex: hack1,hack2...."
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Password"
+          id="password"
+          autoComplete="current-password"
+          type={showPassword ? "text" : "password"}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+          startIcon={<AddIcon />}
+        >
+          Create a Team
+        </Button>
+      </Box>
+    </Box>
+  );
 }
 
 export default CreateTeam;
